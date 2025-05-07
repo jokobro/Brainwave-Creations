3be39bb34b9e91040fe0899ac6255b676ebaf1dd
Robi-1224 <230087@student.glu.nl>
2025-05-07 11:15:21 +0200

    enemy basic pathing and collision detection

---
+++ Brainwave Creations/Assets/Devs/Robin/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyPathing();
+    }
+
+    private void EnemyPathing()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+
+        }else if(collision.gameObject.CompareTag("Side wall"))
+        {
+           
+        }
+    }
+}
