921a720b5f6fadb880ae1e07791f31f5eb778218
Robi-1224 <230087@student.glu.nl>
2025-05-09 09:51:52 +0200

    camer zoom out behaviour working

--- Brainwave Creations/Assets/Devs/Robin/Scripts/CatapultBehaviour.cs
+++ Brainwave Creations/Assets/Devs/Robin/Scripts/CatapultBehaviour.cs
@@ -6,17 +6,25 @@ public class CatapultBehaviour : MonoBehaviour
 {
     private HingeJoint2D joint;
     private JointMotor2D motor;
+    private Camera mainCamera;
+    private float defaultCameraSize;
 
     [Header("Motor properties")]
     [SerializeField] private float motorSpeed;
     [SerializeField] private float motorForce;
     [SerializeField] private float resetTimer;
 
+    [Header("zoom out properties")]
+    [SerializeField] private float waitForZoomOut;
+    [SerializeField] float zoomOutAmount;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
         joint = GetComponent<HingeJoint2D>();
         motor = joint.motor;
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        defaultCameraSize = mainCamera.orthographicSize;
     }
     // turns the motor of the hingeJoint off and applies all the multiplier variables, and then turns it on to apply all of it at once.
     private void LaunchCatapult()
@@ -41,13 +49,23 @@ public class CatapultBehaviour : MonoBehaviour
     {
         // for the player to use during platforming
         if (gameObject.name == "Player catapult")
-        {  
+        {
+            StartCoroutine(CameraZoomOut());
             LaunchCatapult();
         }
         // to slingshot bombs at the enemy structure
         else if(gameObject.name == "Bomb catapult" && collision.gameObject.tag == "Bomb")
         {
+            StartCoroutine(CameraZoomOut());
             LaunchCatapult();
         }
+    }
+
+    private IEnumerator CameraZoomOut()
+    {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(waitForZoomOut);
+        mainCamera.orthographicSize = defaultCameraSize +zoomOutAmount;
+        yield return waitForSeconds;
+        mainCamera.orthographicSize = defaultCameraSize;
     }
 }
