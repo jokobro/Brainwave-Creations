f5921319b273243bfaec6048cc4e35f118b1a938
jokobro <jochemstarwarsclonewars@gmail.com>
2025-05-09 10:28:43 +0200

    added the art to the victory/game over screens

---
+++ Brainwave Creations/Assets/Devs/Jochem/Scripts/VictoryRestart.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+public class VictoryRestart : MonoBehaviour
+{
+    private UIDocument UIDocument;
+    private Button restartButton;
+    private Button exitButton;
+
+
+    private void Awake()
+    {
+        UIDocument = GetComponent<UIDocument>();
+        restartButton = UIDocument.rootVisualElement.Q("RestartButton") as Button;
+        restartButton.RegisterCallback<ClickEvent>(OnrestartClickEvent);
+        exitButton = UIDocument.rootVisualElement.Q("ExitButton") as Button;
+        exitButton.RegisterCallback<ClickEvent>(OnExitClickEvent);
+    }
+
+    private void OnDisable()
+    {
+        restartButton.UnregisterCallback<ClickEvent>(OnrestartClickEvent);
+        exitButton.UnregisterCallback<ClickEvent>(OnExitClickEvent);
+    }
+
+    private void OnrestartClickEvent(ClickEvent clickEvent)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnExitClickEvent(ClickEvent clickEvent) 
+    {
+        SceneManager.LoadScene(0);    
+    }
+
+}
