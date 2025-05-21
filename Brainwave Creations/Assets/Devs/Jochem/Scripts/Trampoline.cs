852ca7ff35b2bc3150697d92239f0f513004df10
jokobro <jochemstarwarsclonewars@gmail.com>
2025-05-21 13:43:55 +0200

    switch statement updated

--- Brainwave Creations/Assets/Devs/Jochem/Scripts/Trampoline.cs
+++ Brainwave Creations/Assets/Devs/Jochem/Scripts/Trampoline.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class Trampoline : MonoBehaviour
 {
     [SerializeField] private float jumpforce;
-    [SerializeField]Transform direction;
+    [SerializeField] Transform direction;
     Rigidbody2D playerRigidbody;
     PlayerController playerController;
+    private float launchForce = 10f;
+    Vector3 launchDirection = Vector3.up;
+
     private void Awake()
     {
         playerRigidbody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
@@ -14,12 +17,23 @@ public class Trampoline : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        switch (gameObject.name)
         {
-            playerController.slinging = true;
-            Vector2 vectorDirection = direction.position - transform.position;
-            if(vectorDirection.x != 0 || vectorDirection.y < 0) playerController.enabled= false;
-            playerRigidbody.AddForce(jumpforce * vectorDirection.normalized,ForceMode2D.Impulse);
+            case "DirectionalLaunchpad":
+                if (collision.gameObject.CompareTag("Player"))
+                {
+                    playerController.slinging = true;
+                    Vector2 vectorDirection = direction.position - transform.position;
+                    if (vectorDirection.x != 0 || vectorDirection.y < 0) playerController.enabled = false;
+                    playerRigidbody.AddForce(jumpforce * vectorDirection.normalized, ForceMode2D.Impulse);
+                }
+                break;
+            case "HeightDependingLaunchpad":
+                if (collision.gameObject.CompareTag("Player"))
+                {
+                    playerRigidbody.AddForce(launchForce * launchDirection);
+                }
+                break;
         }
     }
 }
